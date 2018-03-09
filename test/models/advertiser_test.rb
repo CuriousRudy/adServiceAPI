2cc69a2ea8b587763cbcfe264d9c0932bc12f78d
Abe Rudy <aberudy@gmail.com>
2018-03-09 11:02:42 -0500

    add tests for model validations.

--- test/models/advertiser_test.rb
+++ test/models/advertiser_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class AdvertiserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+    test "should not save advertiser without name" do
+      advertiser = Advertiser.new
+      assert_not advertiser.save
+    end
+
+    test "advertiser names are unique" do
+      advertiser = Advertiser.create(name: "Big advertising")
+      advertiser_2 = Advertiser.new(name: "Big advertising")
+      assert_not advertiser_2.save
+    end
+
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
+
+
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save product without attributes" do
+    product = Product.new
+    assert_not product.save
+  end
+
+  test "should not save product without advertiser" do
+    product = Product.new(product_name: "Television", product_sku: "this is my sku")
+    assert_not product.save
+  end
+
+  test "should save when created completely" do
+    advert = Advertiser.create(name: "Big Advertising")
+    product =  Product.new(product_name: "Television", product_sku: "this is my sku")
+    product.advertiser = advert
+    assert_equal product.advertiser.name, advert.name
+  end
+
 end
