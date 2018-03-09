9cbcacc4d0a588fc69b9b9bea11685fb26113986
Abe Rudy <aberudy@gmail.com>
2018-03-09 16:55:09 -0500

    add cache to index call

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::ProductsController < ApplicationController
 
   def index
+    expires_in 6.hours, public: false
     @products = Product.all
     render json: ProductSerializer.new(@products)
   end
