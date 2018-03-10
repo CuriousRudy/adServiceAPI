342e26b6f0d27995a2a5a1295e684eade545104e
Abe Rudy <aberudy@gmail.com>
2018-03-10 14:53:20 -0500

    add logic for the

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,16 +1,25 @@
 class Api::V1::ProductsController < ApplicationController
 
   @@Start = 1
+  @@Product_count = Product.count
 
+  # sends a header to keep count for each 1000 records
   def index
     @products = Product.all
     render json: {current_index: @@Start + 1000, products: [@products[0...1000]]}
   end
 
+  # includes the logic to go through each index
   def more
-    index = index_to_show.to_i
-    new_index = index.to_i + 1000
-    show_until = index.to_i + 999
+    if @@Product_count - index_to_show.to_i <= 1000
+      index = index_to_show.to_i
+      new_index = @@Product_count
+      show_until = index_to_show.to_i + (@@Product_count - index_to_show.to_i).abs
+    else
+      index = index_to_show.to_i
+      new_index = index + 1000
+      show_until = index + 999
+    end
     @products = Product.all
     # binding.pry
     render json: {current_index: new_index, products: [@products[index - 1...show_until]]}
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
 
+  # read the header with the current index
   def index_to_show
     index = request.headers['HTTP_CURRENT_INDEX']
   end
